35c125aec647a715311114f88e14b6a26f80f2b3
prashant <prim19n@gmail.com>
2023-03-26 20:29:49 +0530

    hospitality

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function () {
+    $("button").click(function () {
+      const element = document.getElementById("luxuryRooms");
+      element.scrollIntoView();
+    });
+  });
